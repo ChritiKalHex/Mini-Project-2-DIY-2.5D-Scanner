d4f1345033e1d804f26d07d74720128b649661da
awoolf07 <awoolf07@gmail.com>
2026-09-15 14:18:12 -0400

    Add initial Arduino scanner and python grapher files

---
+++ grapher.py
@@ -0,0 +1,9 @@
+""" Visualize 2.5D IR scanner data """
+
+def graph_data(file):
+    """
+    Graphs the scanned data as a point cloud in 3D space.
+
+    Args:
+        file (str): The path to the file containing the scanned data.
+    """
