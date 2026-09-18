b3ab97599e6604406b1c3f55ed9467c4f0fa1da8
awoolf07 <awoolf07@gmail.com>
2026-09-18 16:28:01 -0400

    Started on IR reading

--- scanner/scanner.ino
+++ scanner/scanner.ino
@@ -1,9 +1,9 @@
-///////////////////////////////////////////////////////////////////////
-//                                                                   //
-//   Use two servos and a lidar distance sensor to scan and object   //
-//   send the data to a python program to be visualized.             //
-//                                                                   //
-///////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//   Use two servos and a IR distance sensor to scan and object   //
+//   send the data to a python program to be visualized.          //
+//                                                                //
+////////////////////////////////////////////////////////////////////
 
 #include <Servo.h>
 
@@ -14,6 +14,10 @@ uint8_t YAW_PORT = 3;
 uint8_t PITCH_PORT = 5;
 int8_t INCREMENT = 5;
 
+// Set the IR constatns.
+uint8_t IR_PORT = 0;
+float VOLTS_TO_METERS = 1;
+
 // Initialize the servos and their positions.
 Servo yaw_servo;
 int8_t yaw = RANGE_MIN;
@@ -22,11 +26,11 @@ int8_t pitch = RANGE_MIN;
 bool yaw_backwards = false;
 
 void setup() {
-  // Start the serial port.
+  // Setup the serial port.
   long baudRate = 9600;       // The baudRate for sending & receiving programs must match
   Serial.begin(baudRate);     // Set the baudRate to 115200 for faster communication
 
-  // Start the servos.
+  // Setup the servos.
   yaw_servo.attach(YAW_PORT);
   pitch_servo.attach(PITCH_PORT);
 }
@@ -36,8 +40,14 @@ void loop() {
   yaw_servo.write(yaw);
   pitch_servo.write(pitch);
 
-  // Scan with the lidar.
-  uint8_t distance = 1;
+  // Scan with the IR sensor.
+  uint16_t scan1 = analogRead(IR_PORT);
+  uint16_t scan2 = analogRead(IR_PORT);
+  uint16_t scan3 = analogRead(IR_PORT);
+
+  // Claculate the distance.
+  uint16_t voltage = min(min(scan1, scan2), scan3);
+  float distance = voltage * VOLTS_TO_METERS;
 
   // Transmit one line of text to python with the pitch, yaw, and distance values.
   // NOTE: commas are sent between values, after the last value a Newline is sent.
